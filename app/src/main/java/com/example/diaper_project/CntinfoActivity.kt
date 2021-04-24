dd5d0599355c91d72b74249a55fe6a71dfa084fa
chaminwoo <12zxccha@naver.com>
2021-04-24 21:22:59 +0900

    이용자 정보 액티비티(CntinfoActivity) 만듬 - MainActivity에서 이용자 모두 조회한 결과값(jsonarray)을 인텐트에 실어서 받아와서 재사용함. 서버 접근 횟수 줄이기 위함. (이용자 삭제하기 로직 앞으로 추가해줘야함)

---
+++ app/src/main/java/com/example/diaper_project/CntinfoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.diaper_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CntinfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cntinfo)
+    }
+}
